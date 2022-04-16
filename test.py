d13308a17809f3727d9058bc1865ef4cda2fcd7f
Hoale <hoa171996@gmail.com>
2022-04-16 22:57:33 +0700

    first_commit

---
+++ test.py
@@ -0,0 +1,33 @@
+# Operator (toán tử)
+
+# + - Cộng trừ rất thoải mái
+# print ( 1+100 )
+# print ( 100-25 )
+
+#Chia (devide) Chia hết = slash / 
+# print (1/0) #XẢY RA EXCEPTION 1:0
+# ZeroDivisionError
+
+# chia lấy dư ( modulo (%) 
+# print (5%2)
+
+#Chia làm tròn = floor division //
+
+# print (6//4)
+
+# các kiểu số trong python (data type )
+# Nguyên dương - (integer) - int ;
+# 1 2 3 4 5 .... 123123123 -123123123 
+
+# print (type (69))
+
+# kiểu số thực = FLoat
+# KHÔNG BAO GIỜ SO SÁNH Ở KIỂU FLOAT
+# SỐ THỰC CHỈ GẦN ĐÚNG
+# VD
+# if 0.3 + 0.3 == 0.6:
+    # print ("OK")
+# else:
+    # print ("KHÔNG SO SANH DC")
+
+print ('Hoale')
